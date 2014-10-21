437db7950b2b4bdb0c896c297a634e8ff4252ded
Alexander Lomov <lomov.as@gmail.com>
2014-10-21 15:21:37 +0300

    add config dir option

--- hooks/common.sh
+++ hooks/common.sh
@@ -8,6 +8,7 @@ BASEURL="https://download.elasticsearch.org/logstash/logstash"
 CHECKSUM="6e6be734392e8c7d6d55e23d0bd8d2b3"
 WORKDIR="/opt"
 BASEPATH="/opt/logstash"
+CONFIG_DIR="${BASEPATH}/conf.d"
 USER="logstash"
 HOST=`unit-get private-address`
 
